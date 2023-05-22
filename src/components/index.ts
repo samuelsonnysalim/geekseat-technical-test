0dc90744178ab78e20ba5d9d58fecfc3c2c43c99
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-23 04:20:04 +0700

    [RN] Update component index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export {default as PeopleCard} from './PeopleCard';
 export {default as Initial} from './Initial';
+export {default as LabelValue} from './LabelValue';
+export {default as LabelMultipleValues} from './LabelMultipleValues';
