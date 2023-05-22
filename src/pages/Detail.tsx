1149e6460d8c02896fafc669bb56b0d7e4d6494f
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-23 04:20:53 +0700

    [RN] Update Detail Page

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -7,27 +7,97 @@ import {
   Text,
   View,
 } from 'react-native';
+import moment from 'moment';
 import {
   NavigationProp,
   RouteProp,
   useNavigation,
   useRoute,
 } from '@react-navigation/native';
-import {useQuery} from '@tanstack/react-query';
-import {PeopleService} from '@geekseat/technical-test/services';
-import {Initial} from '@geekseat/technical-test/components';
+import {useQueries, useQuery} from '@tanstack/react-query';
+import {
+  FilmService,
+  PeopleService,
+  PlanetService,
+  SpeciesService,
+  StarshipService,
+  VehicleService,
+} from '@geekseat/technical-test/services';
+import {
+  Initial,
+  LabelMultipleValues,
+  LabelValue,
+} from '@geekseat/technical-test/components';
 
 type Params = {Detail: Partial<{id: string; name: string}>};
 export type DetailNavigationProp = NavigationProp<Params>;
 export type DetailRouteProp = RouteProp<Params>;
 
+const useQueryForMultipleValues = <S extends (options: any) => Promise<any>>(
+  urls: string[],
+  path: string,
+  service: S,
+) => {
+  return useQueries({
+    queries:
+      urls.map(url => {
+        const filmId = url.substring(
+          url.indexOf(`${path}/`) + `${path}/`.length,
+          url.length - 1,
+        );
+        return {
+          queryKey: [path, filmId],
+          queryFn: () => service({params: {id: filmId}}),
+        };
+      }) || [],
+  });
+};
+
 export default function Detail(): JSX.Element {
   const navigation = useNavigation<DetailNavigationProp>();
   const route = useRoute<DetailRouteProp>();
-  const {isLoading, error, data} = useQuery({
+  const {
+    isLoading: isPeopleLoading,
+    error: peopleError,
+    data: peopleData,
+  } = useQuery({
     queryKey: ['people', route.params.id],
     queryFn: () => PeopleService.getPeople({params: {id: route.params.id}}),
   });
+
+  const planetId = peopleData?.homeworld.substring(
+    peopleData?.homeworld.indexOf('planets/') + 'planets/'.length,
+    peopleData?.homeworld.length - 1,
+  );
+  const {isLoading: isPlanetLoading, data: planetData} = useQuery({
+    queryKey: planetId ? ['planets', planetId] : [],
+    queryFn: () =>
+      planetId ? PlanetService.getPlanet({params: {id: planetId}}) : {name: ''},
+  });
+
+  const filmResults = useQueryForMultipleValues(
+    peopleData?.films || [],
+    'films',
+    FilmService.getFilm,
+  );
+
+  const speciesResults = useQueryForMultipleValues(
+    peopleData?.species || [],
+    'species',
+    SpeciesService.getSpecies,
+  );
+
+  const vehicleResults = useQueryForMultipleValues(
+    peopleData?.vehicles || [],
+    'vehicles',
+    VehicleService.getVehicle,
+  );
+
+  const starshipResults = useQueryForMultipleValues(
+    peopleData?.starships || [],
+    'starships',
+    StarshipService.getStarship,
+  );
 
   useEffect(() => {
     if (route.params.name) {
@@ -45,14 +115,61 @@ export default function Detail(): JSX.Element {
     }
   }, [route.params.name, navigation]);
 
-  if (isLoading) {
+  if (isPeopleLoading && isPlanetLoading) {
     return <ActivityIndicator style={styles.singleItem} size="large" />;
-  } else if (!data && error) {
-    return <Text style={styles.singleItem}>{(error as Error).message}</Text>;
+  } else if (!peopleData && peopleError) {
+    return (
+      <Text style={styles.singleItem}>{(peopleError as Error).message}</Text>
+    );
   } else {
     return (
       <ScrollView style={styles.scrollView}>
-        <View style={styles.container} />
+        <View style={styles.container}>
+          <LabelValue label="Height" value={`${peopleData?.height}cm`} />
+          <LabelValue label="Mass" value={`${peopleData?.mass}kg`} />
+          <LabelValue label="Hair Color" value={peopleData?.hair_color} />
+          <LabelValue label="Skin Color" value={peopleData?.skin_color} />
+          <LabelValue label="Eye Color" value={peopleData?.eye_color} />
+          <LabelValue label="Birth Year" value={peopleData?.birth_year} />
+          <LabelValue label="Gender" value={peopleData?.gender} />
+          <LabelValue label="Homeworld" value={planetData?.name} />
+          <LabelMultipleValues
+            label="Films"
+            values={filmResults.map(result => ({
+              title: result.data?.title,
+              description: result.data?.opening_crawl,
+            }))}
+          />
+          <LabelMultipleValues
+            label="Species"
+            values={speciesResults.map(result => ({
+              title: result.data?.name,
+              description: result.data?.classification,
+            }))}
+          />
+          <LabelMultipleValues
+            label="Vehicles"
+            values={vehicleResults.map(result => ({
+              title: result.data?.name,
+              description: result.data?.manufacturer,
+            }))}
+          />
+          <LabelMultipleValues
+            label="Starship"
+            values={starshipResults.map(result => ({
+              title: result.data?.name,
+              description: result.data?.manufacturer,
+            }))}
+          />
+          <LabelValue
+            label="Created At"
+            value={moment(peopleData?.created).format('DD-MM-YYYY')}
+          />
+          <LabelValue
+            label="Edited At"
+            value={moment(peopleData?.edited).format('DD-MM-YYYY')}
+          />
+        </View>
       </ScrollView>
     );
   }
@@ -67,7 +184,6 @@ const styles = StyleSheet.create({
   },
   container: {
     padding: 16,
-    minHeight: '100%',
   },
   containerInitial: {
     marginRight: 12,
--- src/pages/__tests__/Detail.test.tsx
+++ src/pages/__tests__/Detail.test.tsx
@@ -27,24 +27,57 @@ jest.mock('@geekseat/technical-test/services/PeopleService', () => ({
       birth_year: '19BBY',
       gender: 'male',
       homeworld: 'https://swapi.dev/api/planets/1/',
-      films: [
-        'https://swapi.dev/api/films/1/',
-        'https://swapi.dev/api/films/2/',
-        'https://swapi.dev/api/films/3/',
-        'https://swapi.dev/api/films/6/',
-      ],
-      species: [],
-      vehicles: [
-        'https://swapi.dev/api/vehicles/14/',
-        'https://swapi.dev/api/vehicles/30/',
-      ],
-      starships: [
-        'https://swapi.dev/api/starships/12/',
-        'https://swapi.dev/api/starships/22/',
-      ],
+      films: ['https://swapi.dev/api/films/1/'],
+      species: ['https://swapi.dev/api/species/1/'],
+      vehicles: ['https://swapi.dev/api/vehicles/14/'],
+      starships: ['https://swapi.dev/api/starships/12/'],
       created: '2014-12-09T13:50:51.644000Z',
       edited: '2014-12-20T21:17:56.891000Z',
       url: 'https://swapi.dev/api/people/1/',
+    }),
+  },
+}));
+
+jest.mock('@geekseat/technical-test/services/PlanetService', () => ({
+  PlanetService: {
+    getPlanet: jest.fn().mockResolvedValue({
+      name: 'Tatooine',
+    }),
+  },
+}));
+
+jest.mock('@geekseat/technical-test/services/FilmService', () => ({
+  FilmService: {
+    getFilm: jest.fn().mockResolvedValue({
+      title: 'A New Hope',
+      opening_crawl: 'It is a period of civil war.',
+    }),
+  },
+}));
+
+jest.mock('@geekseat/technical-test/services/SpeciesService', () => ({
+  SpeciesService: {
+    getSpecies: jest.fn().mockResolvedValue({
+      name: 'Human',
+      classification: 'mammal',
+    }),
+  },
+}));
+
+jest.mock('@geekseat/technical-test/services/VehicleService', () => ({
+  VehicleService: {
+    getVehicle: jest.fn().mockResolvedValue({
+      name: 'Snowspeeder',
+      manufacturer: 'Incom corporation',
+    }),
+  },
+}));
+
+jest.mock('@geekseat/technical-test/services/StarshipService', () => ({
+  StarshipService: {
+    getStarship: jest.fn().mockResolvedValue({
+      name: 'X-wing',
+      manufacturer: 'Incom corporation',
     }),
   },
 }));
@@ -61,7 +94,54 @@ describe('Detail', () => {
       </QueryClientProvider>,
     );
     await waitFor(() => {
+      expect(screen.getByText('LS')).toBeDefined();
       expect(screen.getByText('Luke Skywalker')).toBeDefined();
+
+      expect(screen.getByText('Height')).toBeDefined();
+      expect(screen.getByText('172cm')).toBeDefined();
+
+      expect(screen.getByText('Mass')).toBeDefined();
+      expect(screen.getByText('77kg')).toBeDefined();
+
+      expect(screen.getByText('Hair Color')).toBeDefined();
+      expect(screen.getByText('blond')).toBeDefined();
+
+      expect(screen.getByText('Skin Color')).toBeDefined();
+      expect(screen.getByText('fair')).toBeDefined();
+
+      expect(screen.getByText('Eye Color')).toBeDefined();
+      expect(screen.getByText('blue')).toBeDefined();
+
+      expect(screen.getByText('Birth Year')).toBeDefined();
+      expect(screen.getByText('19BBY')).toBeDefined();
+
+      expect(screen.getByText('Gender')).toBeDefined();
+      expect(screen.getByText('male')).toBeDefined();
+
+      expect(screen.getByText('Homeworld')).toBeDefined();
+      expect(screen.getByText('Tatooine')).toBeDefined();
+
+      expect(screen.getByText('Films')).toBeDefined();
+      expect(screen.getByText('• A New Hope')).toBeDefined();
+      expect(screen.getByText('It is a period of civil war.')).toBeDefined();
+
+      expect(screen.getByText('Species')).toBeDefined();
+      expect(screen.getByText('• Human')).toBeDefined();
+      expect(screen.getByText('mammal')).toBeDefined();
+
+      expect(screen.getByText('Vehicles')).toBeDefined();
+      expect(screen.getByText('• Snowspeeder')).toBeDefined();
+      expect(screen.getAllByText('Incom corporation')[0]).toBeDefined();
+
+      expect(screen.getByText('Vehicles')).toBeDefined();
+      expect(screen.getByText('• X-wing')).toBeDefined();
+      expect(screen.getAllByText('Incom corporation')[1]).toBeDefined();
+
+      expect(screen.getByText('Created At')).toBeDefined();
+      expect(screen.getByText('09-12-2014')).toBeDefined();
+
+      expect(screen.getByText('Edited At')).toBeDefined();
+      expect(screen.getByText('21-12-2014')).toBeDefined();
     });
   });
 });
