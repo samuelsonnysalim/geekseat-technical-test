4820c20cad02d9e464c26aabddfd979315833aee
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-22 17:02:44 +0700

    [RN] Refactor App and add initialRouteName as a prop

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,30 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '@geekseat/technical-test/pages/Home';
 
+interface AppNavigatorProps {
+  initialRouteName: string;
+}
+
 const queryClient = new QueryClient();
 const Stack = createNativeStackNavigator();
+
+export function AppNavigator(props: Partial<AppNavigatorProps>): JSX.Element {
+  return (
+    <Stack.Navigator initialRouteName={props.initialRouteName}>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{title: 'Star Wars Characters'}}
+      />
+    </Stack.Navigator>
+  );
+}
 
 function App(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
+        <AppNavigator />
       </NavigationContainer>
     </QueryClientProvider>
   );
