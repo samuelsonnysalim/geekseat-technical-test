ffbd8e1336e6656e31f8434c2d00c2cdb026371b
Samuel Sonny Salim <samuel.sonny.salim@gmail.com>
2023-05-22 19:09:25 +0700

    [RN] Encapsulate pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Home from '@geekseat/technical-test/pages/Home';
+import {Home} from '@geekseat/technical-test/pages';
 
 interface AppNavigatorProps {
   initialRouteName: string;
